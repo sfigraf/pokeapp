d62242bc3a44fb7fbc84d36aa611f115713e0ed1
sfigraf <sfigraf@gmail.com>
2019-08-02 16:47:43 -0400

    Improved percentile function

--- app.R
+++ app.R
@@ -13,20 +13,13 @@ library(shiny)
 pokemon_sheet <- read.csv("Pokemon.csv")
 #can rank pokemon too 
 #creates functions 'percentile' that gets the percentile of a column
-percentileattack = ecdf(min(pokemon_sheet$Attack):max(pokemon_sheet$Attack))
-percentilehp = ecdf(min(pokemon_sheet$HP):max(pokemon_sheet$HP))
-percentiledefense = ecdf(min(pokemon_sheet$Defense):max(pokemon_sheet$Defense))
-percentileSpatt = ecdf(min(pokemon_sheet$Sp..Atk):max(pokemon_sheet$Sp..Atk))
-percentileaSp.def = ecdf(min(pokemon_sheet$Sp..Def):max(pokemon_sheet$Sp..Def))
-percentileSpeed = ecdf(min(pokemon_sheet$Speed):max(pokemon_sheet$Speed))
-#create new columns that have percentages of stats for the pokemons
 new_cols <- pokemon_sheet %>%
-  mutate(HP.percentile = percentilehp(HP),
-         Attack.percentile = percentileattack(Attack),
-         Defense.percentile = percentiledefense(Defense),
-         Sp.Attackpercentile = percentileSpatt(Sp..Atk),
-         Sp.Defensepercentile = percentileaSp.def(Sp..Def),
-         Speed.percentile = percentileSpeed(Speed)) %>%
+  mutate(Attack.percentile = ntile(Attack,100),
+         HP.percentile = ntile(HP,100),
+         Defense.percentile = ntile(Defense,100),
+         Sp.Attackpercentile = ntile(Sp..Atk,100),
+         Sp.Defensepercentile = ntile(Sp..Def,100),
+         Speed.percentile = ntile(Speed,100)) %>%
   select(Name,
          HP.percentile,
          Attack.percentile,
@@ -34,6 +27,30 @@ new_cols <- pokemon_sheet %>%
          Sp.Attackpercentile,
          Sp.Defensepercentile,
          Speed.percentile) 
+# y <- x %>%
+#   mutate(percentileattack2 = quantile(x$Attack, probs = c(0,.25,.5,.75,1)))
+# ?quantile
+# percentileattack = ecdf(min(pokemon_sheet$Attack):max(pokemon_sheet$Attack))
+# percentilehp = ecdf(min(pokemon_sheet$HP):max(pokemon_sheet$HP))
+# percentiledefense = ecdf(min(pokemon_sheet$Defense):max(pokemon_sheet$Defense))
+# percentileSpatt = ecdf(min(pokemon_sheet$Sp..Atk):max(pokemon_sheet$Sp..Atk))
+# percentileaSp.def = ecdf(min(pokemon_sheet$Sp..Def):max(pokemon_sheet$Sp..Def))
+# percentileSpeed = ecdf(min(pokemon_sheet$Speed):max(pokemon_sheet$Speed))
+#create new columns that have percentages of stats for the pokemons
+# new_cols <- pokemon_sheet %>%
+#   mutate(HP.percentile = percentilehp(HP),
+#          Attack.percentile = percentileattack(Attack),
+#          Defense.percentile = percentiledefense(Defense),
+#          Sp.Attackpercentile = percentileSpatt(Sp..Atk),
+#          Sp.Defensepercentile = percentileaSp.def(Sp..Def),
+#          Speed.percentile = percentileSpeed(Speed)) %>%
+#   select(Name,
+#          HP.percentile,
+#          Attack.percentile,
+#          Defense.percentile,
+#          Sp.Attackpercentile,
+#          Sp.Defensepercentile,
+#          Speed.percentile) 
 
 #Map stuff
 states.url <- "http://eric.clst.org/assets/wiki/uploads/Stuff/gz_2010_us_040_00_5m.json"
@@ -102,7 +119,7 @@ server <- function(input, output, session) {
   
   
   output$plot1 <- renderPlot({
-    input$Search
+    req(input$Search)
     row <- which(grepl(input$text1, new_cols$Name)) #searches name row and returns the row position where it's found
     x <- new_cols[row,] #makes data frame with just that pokemon's stats
     #gathers data to one row
